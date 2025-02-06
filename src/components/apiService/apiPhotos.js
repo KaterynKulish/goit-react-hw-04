5156040c3f3c3fa515d4177ef1e9420ce3222c6c
Kateryna Kulish <kulishk26@gmail.com>
2025-02-06 20:01:03 +0200

    HW-4

--- src/components/apiService/apiPhotos.js
+++ src/components/apiService/apiPhotos.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const API_KEY = 'byqm53SSK-L4Jv3pruPB3aSmT7W9peOt8UrDpLC2JpY';
+
 const fetchPhotos = async (query, page, perPage) => {
   // const res = await axios.get(
   //   `https://api.unsplash.com/search/photos?client_id=${
@@ -7,7 +9,7 @@ const fetchPhotos = async (query, page, perPage) => {
   //   }&orientation=landscape&query=${query}&page=${page}&per_page=${perPage}`
   // );
   const res = await axios.get(
-    `https://api.unsplash.com/search/photos?client_id=byqm53SSK-L4Jv3pruPB3aSmT7W9peOt8UrDpLC2JpY&orientation=landscape&query=${query}&page=${page}&per_page=${perPage}`
+    `https://api.unsplash.com/search/photos?client_id=${API_KEY}&orientation=landscape&query=${query}&page=${page}&per_page=${perPage}`
   );
   console.log(res);
   console.log(res.data);
